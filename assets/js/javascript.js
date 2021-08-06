ce1eaa6fe09ea58a3c1659c31ec91f5dde4be47e
arthur4ires <aires.arthur.15@gmail.com>
2021-08-06 17:59:40 -0300

    fix js

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -4,33 +4,33 @@ $(document).ready(function(){
   $('.percentages section:nth-child(1) > .progress-bar').circleProgress({
     startAngle: -1.55,
     size: 120,
-      value: 1.00,
+      value: 100,
       fill: {
       color: 'crimson'
     }
   }).on('circle-animation-progress', function(event, progress, stepValue) {
-    $(this).find('strong').text(String(stepValue.toFixed(2)).substr(1));
+    $(this).find('strong').text(String(stepValue.toFixed(0)).substr(0));
   });
 
   $('.percentages section:nth-child(2) .progress-bar').circleProgress({
     startAngle: -1.55,
     size: 120,
-      value: 1.00,
+      value: 100,
       fill: {
       color: 'crimson'
     }
   }).on('circle-animation-progress', function(event, progress, stepValue) {
-    $(this).find('strong').text(String(stepValue.toFixed(2)).substr(1));
+    $(this).find('strong').text(String(stepValue.toFixed(0)).substr(0));
   });
 
   $('.percentages section:nth-child(3) .progress-bar').circleProgress({
     startAngle: -1.55,
     size: 120,
-      value: 1.00,
+      value: 100,
       fill: {
       color: 'crimson'
     }
   }).on('circle-animation-progress', function(event, progress, stepValue) {
-    $(this).find('strong').text(String(stepValue.toFixed(2)).substr(1));
+    $(this).find('strong').text(String(stepValue.toFixed(0)).substr(0));
   });
 });
